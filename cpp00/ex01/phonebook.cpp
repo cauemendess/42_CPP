b8a6674da3f687657082406f2251b855f8dd1887
Cauê Silva Mendes <caue_1456mendes@outlook.com>
2024-10-31 14:39:12 -0300

    WIP: Ex01

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <string>
 
-int main()
+int main(void)
 {
-    
+    std::string command;
+    while(1)
+    {
+        std::cout << "Enter a command: ";
+        std::getline(std::cin, command);
+        if(command == "ADD")
+        {
+            
+            continue ;
+        }
+        else if(command == "SEARCH")
+
+
+        if(input == "EXIT")
+            break ;
+    }
+    std::cout << "Program terminated." << std::endl;
 }
