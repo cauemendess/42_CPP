0c949fb1afcf9a748a09566961eb4f7ca2bbc066
Cauê Silva Mendes <caue_1456mendes@outlook.com>
2024-12-03 23:06:07 -0300

    Add ex05 on Module 1

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -1,5 +1,20 @@
 #include "Weapon.hpp"
 
+Weapon::Weapon(std::string type) : _type(type)
+{
+	return ;
+}
+
+Weapon::~Weapon()
+{
+	return ;
+}
+
+std::string const &Weapon::getType()
+{
+	return this->_type;
+}
+
 void Weapon::setType(std::string type)
 {
 	_type = type;
