7a8244ee21fba19ae26666d2f3b5c97a8e4ec1d2
Cauê Mendes <caue_1456mendes@outlook.com>
2025-07-18 15:47:12 -0300

    Module 09: finish!

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     if (argc != 2)
     {
         std::cerr << "Usage: " << argv[0] << " <expression>" << std::endl;
-        return 1;
+        return 0;
     }
 
     try
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
     catch (const std::runtime_error &e)
     {
         std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
+        return 0;
     }
 
     return 0;
